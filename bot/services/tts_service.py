dfb047d859ccb8cd979fa0ad2e226828ef78805b
ajr-dev <albertrabie2@gmail.com>
2025-01-20 19:24:42 +0200

    tweak: bark presets comment

--- bot/services/tts_service.py
+++ bot/services/tts_service.py
@@ -39,7 +39,7 @@ class TTSService:
                 processor = AutoProcessor.from_pretrained("suno/bark")
                 model = BarkModel.from_pretrained("suno/bark")
 
-                voice_preset = "v2/en_speaker_6"
+                voice_preset = "v2/en_speaker_6" # https://suno-ai.notion.site/8b8e8749ed514b0cbf3f699013548683?v=bc67cff786b04b50b3ceb756fd05f68c
 
                 inputs = processor(text, voice_preset=voice_preset)
 
